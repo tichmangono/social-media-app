8ef1f890aa3c537cc0524bcff5e92bd5a50437cb
Tichakunda Mangono <tichmangono@gmail.com>
2020-11-25 23:04:24 -0500

    main index files

---
+++ app/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Main() {
+  return (
+    <div>
+      <h1>This is our app</h1>
+      <p>The sky is blue</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"))
